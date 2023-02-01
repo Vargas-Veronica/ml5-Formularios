15ea59463d8314df4adeddd8308b7b7a523a7f99
Vargas-Veronica <vargas.veronica55@gmail.com>
2023-02-01 14:15:01 -0300

    corrijo formulario

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.get("/", (req, res) => {
    return res.sendFile(path.join(__dirname, "/views/home.html"));
 })
 
+app.get("/login", (req, res) => {
+   return res.sendFile(path.join(__dirname, "/views/login.html"));
+})
+app.get("/register", (req, res) => {
+   return res.sendFile(path.join(__dirname, "/views/register.html"));
+})
+
 app.listen(PORT, () => console.log(`
 Server listen in port ${PORT}
 http://localhost:${PORT}
